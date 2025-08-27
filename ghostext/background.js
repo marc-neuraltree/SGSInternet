54a5cf7d01e8cf4c6e15b755403e2584a680e435
marc-neuraltree <marc@neuraltree.ai>
2025-08-27 14:57:09 +0700

    Update background.js

--- ghostext/background.js
+++ ghostext/background.js
@@ -6,10 +6,29 @@ let rotationInterval = null;
 
 // Default test sites - modify these for your local testing
 const DEFAULT_SITES = [
-  'http://localhost:3000',
-  'http://localhost:8080',
-  'http://localhost:5000',
-  'http://localhost:4200',
+  'http://amazon.com'
+  'http://linkedin.com'
+  'http://detik.com'
+  'http://microsoft.com'
+  'http://tokopedia.com'
+  'http://kompas.com'
+  'http://kemhan.go.id'
+  'http://kompas.com'
+  'http://gojek.com'
+  'http://indonesia.go.id'
+  'http://tni.mil.id'
+  'http://polri.go.id'
+  'http://kemenkeu.go.id'
+  'http://youtube.com'
+  'http://facebook.com'
+  'http://wikipedia.org'
+  'http://instagram.com'
+  'http://shopee.co.id'
+  'http://traveloka.com'
+  'http://cnnindonesia.com'
+  'http://tribunnews.com'
+  'http://twitter.com'
+  'http://cnbcindonesia.com'
   'https://www.google.com'
 ];
 
